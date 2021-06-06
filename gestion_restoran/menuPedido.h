5466a9677381cefc6fed75e546f5bb402ad63855
Nicolas <nicolasfediuk@gmail.com>
2021-06-06 15:42:47 -0300

    pedido

---
+++ gestion_restoran/menuPedido.h
@@ -0,0 +1,17 @@
+#ifndef MENUPEDIDO_H_INCLUDED
+#define MENUPEDIDO_H_INCLUDED
+
+
+void menuPedido(){
+    int opc;
+    system("cls");
+    tituloAnimado();
+    gotoxy(17,6); cout<<"CLIENTES";
+    gotoxy(58,6); cout<<"PEDIDOS";
+    gotoxy(108,6); cout<<"CERRAR";
+
+
+}
+
+
+#endif // MENUPEDIDO_H_INCLUDED
